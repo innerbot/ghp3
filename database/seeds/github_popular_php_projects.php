b5790dd8c2d1c03399c49606361869a84684e107
Greg Johnson <greg@innerbot.com>
2017-02-08 09:20:34 -0600

    added db seeder stub: github_popular_php_projects.php

---
+++ database/seeds/github_popular_php_projects.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class github_popular_php_projects extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
